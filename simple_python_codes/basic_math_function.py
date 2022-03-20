93003eb782190f5447c67dc625b489b3d7436b92
Gökhan Alkan <94810119+goalkan@users.noreply.github.com>
2022-03-20 14:33:16 +0100

    Add files via upload

---
+++ simple_python_codes/basic_math_function.py
@@ -0,0 +1,8 @@
+from math import*
+my_num1 = 5
+my_num2 = round(9.999999, 3)
+my_num3 = my_num1 - my_num2
+print("value of my third number is " + str(my_num3))
+print("absolute value of " + str(my_num3) + " is " + str(abs(my_num3)) + " which is my favorite number")
+print("please enter to close the window!")
+input()
